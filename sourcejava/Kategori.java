8bcfbfb69225298ebb322c2f2177f576b9f38b29
Kartika Sari <92368462+Kartikasaei@users.noreply.github.com>
2022-12-19 01:22:35 +0700

    Update Kategori.java

--- sourcejava/Kategori.java
+++ sourcejava/Kategori.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package uap;
 
-/**
- *
- * @author lenovo
- */
+package uap_pbo;
+
 public class Kategori {
     private String nama_kategori;
     Barang barang;
