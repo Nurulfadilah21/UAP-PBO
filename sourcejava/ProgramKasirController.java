cc98ee58bb288d67dba250e3138136ce4b6673a6
Kartika Sari <92368462+Kartikasaei@users.noreply.github.com>
2022-12-18 22:47:34 +0700

    Create ProgramKasirController.java

---
+++ sourcejava/ProgramKasirController.java
@@ -0,0 +1,32 @@
+package uap_pbo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ProgramKasirController {
+
+    @FXML
+    private Button btnBrg;
+
+    @FXML
+    private Button btnfood;
+
+    @FXML
+    private Label prgksr;
+
+    @FXML
+    private Label pilMenu;
+
+    @FXML
+    void keBrg(ActionEvent event) {
+
+    }
+
+    @FXML
+    void keMakanan(ActionEvent event) {
+
+    }
+
+}
