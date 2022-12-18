112742c3a3fa4a0be051611f2dff13ce4b08a9c6
Kartika Sari <92368462+Kartikasaei@users.noreply.github.com>
2022-12-19 01:39:08 +0700

    Update Penjualan.java

--- sourcejava/Penjualan.java
+++ sourcejava/Penjualan.java
@@ -1,47 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package uap_pbo;
 
 import java.util.ArrayList;
-
-/**
- *
- * @author Lenovo
- */
     
-public Penjualan(){
+public class Penjualan implements ProductCounter{
+    private ArrayList<Produk> listProduk=new ArrayList<>();
+    private int id_transaksi;
+    private int jumlahProduk;
+    private int stok;
+    private String nama_produk;
+    
+    public Penjualan(){
     }
 
-public Penjualan(int id_transaksi) {
-     this.id_transaksi = id_transaksi;
+    public Penjualan(int id_transaksi) {
+        this.id_transaksi = id_transaksi;
     }
 
-public Penjualan(int id_transaksi, int jumlahProduk, int stok) {
-     this.id_transaksi = id_transaksi;
-     this.jumlahProduk = jumlahProduk;
-     this.stok = stok;
+    public Penjualan(int id_transaksi, int jumlahProduk, int stok) {
+        this.id_transaksi = id_transaksi;
+        this.jumlahProduk = jumlahProduk;
+        this.stok = stok;
     }
-
-public Penjualan(int id_transaksi, int jumlahProduk, int stok, String produk) {
-     this.id_transaksi = id_transaksi;
-     this.jumlahProduk = jumlahProduk;
-     this.stok = stok;
-     this.nama_produk=produk;
+    public Penjualan(int id_transaksi, int jumlahProduk, int stok, String produk) {
+        this.id_transaksi = id_transaksi;
+        this.jumlahProduk = jumlahProduk;
+        this.stok = stok;
+        this.nama_produk=produk;
     }
-
-public Penjualan(int jumlahProduk, int stok) {
-     this.jumlahProduk = jumlahProduk;
-     this.stok = stok;
+    public Penjualan(int jumlahProduk, int stok) {
+        this.jumlahProduk = jumlahProduk;
+        this.stok = stok;
     }
-
-public ArrayList<Produk> getListProduk() {
+    public ArrayList<Produk> getListProduk() {
         return listProduk;
     }
 
-public int getId_transaksi() {
+    public int getId_transaksi() {
         return id_transaksi;
     }
 
