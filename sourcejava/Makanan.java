0801c3d0763b21e31b5c405ee25e52513310fab1
Kartika Sari <92368462+Kartikasaei@users.noreply.github.com>
2022-12-19 01:30:51 +0700

    Update Makanan.java

--- sourcejava/Makanan.java
+++ sourcejava/Makanan.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package uap_pbo;
 
-/**
- *
- * @author Lenovo       
- */
 public class Makanan extends Produk {
     private int id;
     private int daya_tahan;
