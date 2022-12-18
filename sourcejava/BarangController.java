12ec0efffc387d970fc715f098aca6fe3948e39b
Kartika Sari <92368462+Kartikasaei@users.noreply.github.com>
2022-12-18 23:07:23 +0700

    Create BarangController.java

---
+++ sourcejava/BarangController.java
@@ -0,0 +1,57 @@
+package uap_pbo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class BarangController {
+
+    @FXML
+    private Label Brgname;
+
+    @FXML
+    private Label Brgprice;
+
+    @FXML
+    private Label brgJml;
+
+    @FXML
+    private TextField discBrg;
+
+    @FXML
+    private Label expDateBrg;
+
+    @FXML
+    private TextField idBarang;
+
+    @FXML
+    private TextField hargaBarang;
+
+    @FXML
+    private TextField jumlahBrg;
+
+    @FXML
+    private Label barcodeBrg;
+
+    @FXML
+    private TextField expBrg;
+
+    @FXML
+    private Button addBrg;
+
+    @FXML
+    private Button delBrg;
+
+    @FXML
+    void hapusBrg(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tambahData(ActionEvent event) {
+
+    }
+
+}
