7e18c651aa2bf48941ae6f8e1dfce3cbbc221c35
Kartika Sari <92368462+Kartikasaei@users.noreply.github.com>
2022-12-19 00:26:45 +0700

    Create MakananController.java

---
+++ sourcejava/MakananController.java
@@ -0,0 +1,49 @@
+package uap_pbo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class MakananController {
+
+    @FXML
+    private TextField idfood;
+
+    @FXML
+    private TextField idHarga;
+
+    @FXML
+    private TextField idJumlah;
+
+    @FXML
+    private TextField idDiskon;
+
+    @FXML
+    private TextField expFood;
+
+    @FXML
+    private Button addfood;
+
+    @FXML
+    private Button idHps;
+
+    @FXML
+    private Button backpage;
+
+    @FXML
+    void AddBrg(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hpsBrg(ActionEvent event) {
+
+    }
+
+    @FXML
+    void kePrgrmKsr(ActionEvent event) {
+
+    }
+
+}
